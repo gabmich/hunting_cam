6426b8f79e1dae8389f304289f255ced735828fb
gabmichelet <gabmichelet@bluewin.ch>
2023-02-18 16:46:12 +0100

    2nd commit

---
+++ hunting_cam/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for hunting_cam project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hunting_cam.settings')
+
+application = get_asgi_application()
